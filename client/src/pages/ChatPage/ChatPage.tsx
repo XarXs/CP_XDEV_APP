5953b6954970ef17af6f98ee0f19bebf32032ae2
Adam <adam.chabros@itsquad.pl>
2022-10-19 17:31:38 +0200

    CP_XDEV_APP_4: Chmurki wiadomości

--- client/src/pages/ChatPage/ChatPage.tsx
+++ client/src/pages/ChatPage/ChatPage.tsx
@@ -3,13 +3,26 @@ import React from 'react';
 import './ChatPage.scss';
 import InputComponent from '../../components/InputComponent/InputComponent';
 import ButtonComponent from '../../components/ButtonComponent/ButtonComponent';
+import MessageComponent from '../../components/MessageComponent/MessageComponent';
 
 const ChatPage = () => {
   return (
     <div className={'chat-page'}>
       Chat Page
       <div className={'chat-page-container'}>
-        <div className={'chat-page-chat-container'}> ChatContainer </div>
+        <div className={'chat-page-chat-container'}>
+          <MessageComponent type={'user'} label={'user'}>
+            Wiadomość Usera
+          </MessageComponent>
+
+          <MessageComponent type={'other'} label={'other'}>
+            Wiadomość innego usera
+          </MessageComponent>
+
+          <MessageComponent type={'admin'} label={'admin'}>
+            Wiadomość admina.
+          </MessageComponent>
+        </div>
         <form className={'chat-page-input-container'}>
           <InputComponent name={'userInput'} />
           <ButtonComponent type={'submit'}>Login</ButtonComponent>
